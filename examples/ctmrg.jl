34167b82c0a92b66f50cc4ad9af0cb1167ef61f2
YidaiZhang <yzhang958@connect.hkust-gz.edu.cn>
2024-06-01 15:45:10 +0800

    up

--- examples/ctmrg.jl
+++ examples/ctmrg.jl
@@ -3,5 +3,20 @@ using TensorNetworkAD2.OMEinsum
 
 h = hamiltonian(Heisenberg())
 ipeps = SquareIPEPS(rand(2,2,2,2,2));
-ipeps = TensorNetworkAD.indexperm_symmetrize(ipeps);
-TensorNetworkAD.energy(h,ipeps, χ=20, tol=1e-6,maxit=100)
+ipeps = TensorNetworkAD2.indexperm_symmetrize(ipeps);
+TensorNetworkAD2.energy(h,ipeps, χ=20, tol=1e-6,maxit=100)
+
+function energy(h::AbstractArray{T,4}, ipeps::IPEPS; χ::Int, tol::Real, maxit::Int) where T
+    ipeps = indexperm_symmetrize(ipeps)  # NOTE: this is not good
+    D = getd(ipeps)^2
+    s = gets(ipeps)
+    ap = ein"abcdx,ijkly -> aibjckdlxy"(ipeps.bulk, conj(ipeps.bulk))
+    ap = reshape(ap, D, D, D, D, s, s)
+    a = ein"ijklaa -> ijkl"(ap)
+
+    rt = SquareCTMRGRuntime(a, Val(:raw), χ)
+    rt  = ctmrg(rt; tol=tol, maxit=maxit)
+    e = expectationvalue(h, ap, rt)
+    return e
+end
+energy(h,ipeps, χ=20, tol=1e-6,maxit=100)
--- src/TensorNetworkAD2.jl
+++ src/TensorNetworkAD2.jl
@@ -5,17 +5,31 @@ using ITensors
 using OMEinsum
 using TensorOperations
 using QuadGK
+using Random
+using IterTools: iterated
+using Base.Iterators: take, drop
+using Optim, LineSearches
+
+
 
 export TRG
 export trg
 export trg_i
 export ising_mpo, ising_free_energy
 export hamiltonian, Heisenberg
+export IPEPS, SquareIPEPS
+export diaglocalhamiltonian, expectationvalue, optimiseipeps
+export energy, indexperm_symmetrize
+export state 
+
 
 include("trg_to.jl")
 include("trg_ome.jl")
 include("trg_itensors.jl")
 include("2d_ising.jl")
 include("heisenbergmodel.jl")
+include("ipeps.jl")
+include("ctmrg.jl")
+include("variation.jl")
 
 end
